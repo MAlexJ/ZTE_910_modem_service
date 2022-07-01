28c4be37a245263d2bc8c7405b523de9267a55e8
Oleksii Maksymov <alexmaximovj@gmail.com>
2022-07-02 00:11:25 +0300

    Fix tests

--- src/test/java/com/malexj/RunApplicationTests.java
+++ src/test/java/com/malexj/RunApplicationTests.java
@@ -6,6 +6,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RunApplicationTests {
 
+	static {
+		System.setProperty("ADMIN_PHONE_NUMBER", "8067777777");
+	}
+
 	@Test
 	void contextLoads() {
 	}

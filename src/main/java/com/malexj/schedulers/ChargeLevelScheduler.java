1ee3c9e74db09b632befa823831255dabd237ad1
Oleksii Maksymov <alexmaximovj@gmail.com>
2022-07-01 23:57:18 +0300

    Add sheduler impl with notification

--- src/main/java/com/malexj/schedulers/ChargeLevelScheduler.java
+++ src/main/java/com/malexj/schedulers/ChargeLevelScheduler.java
@@ -1,9 +1,11 @@
 package com.malexj.schedulers;
 
+import com.malexj.models.requests.MessageRequest;
 import com.malexj.models.responses.BatteryResponse;
 import com.malexj.services.ZteRestService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -13,14 +15,61 @@ import static com.malexj.controllers.ZteRestController.BATTERY_CHARGING_REQUEST;
 @Component
 @RequiredArgsConstructor
 public class ChargeLevelScheduler {
+    @Value("${scheduled.task.phone}")
+    private String phoneNumber;
+    @Value("${scheduled.task.low.level}")
+    private int lowChargeLevel;
+
+    private boolean isNotified;
 
     private final ZteRestService restService;
 
     @Scheduled(cron = "${scheduled.task.job.cron}")
     public void checkChargeLevel() {
         BatteryResponse info = restService.getInfo(BATTERY_CHARGING_REQUEST);
-        String batteryVolPercent = info.getBatteryVolPercent();
-        log.info(" >>> " + batteryVolPercent);
+        int percent = parsePercent(info.getBatteryVolPercent());
+        log.info("battery percent: " + percent);
+
+        if (percent < lowChargeLevel && !isNotified) {
+            String message = buildLowBatteryMessage(info);
+            log.warning(message);
+            restService.sendMessage(new MessageRequest(phoneNumber, message));
+            isNotified = true;
+            return;
+        }
+
+        if (percent > lowChargeLevel && isNotified) {
+            String message = buildChargeRestoredMessage(info);
+            log.info(message);
+            restService.sendMessage(new MessageRequest(phoneNumber, buildChargeRestoredMessage(info)));
+            isNotified = false;
+        }
+    }
+
+    private String buildLowBatteryMessage(BatteryResponse info) {
+        StringBuilder sb = new StringBuilder("Critically low battery level - ") //
+                .append(info.getBatteryVolPercent()) //
+                .append("charge your GSM modem!");
+        return new String(sb);
+    }
+
+    private String buildChargeRestoredMessage(BatteryResponse info) {
+        StringBuilder sb = new StringBuilder("Battery charge restored") //
+                .append(", ")//
+                .append("percent-")//
+                .append(info.getBatteryVolPercent())//
+                .append(", charging-")//
+                .append(info.getBatteryCharging());
+        return new String(sb);
+    }
+
+    private int parsePercent(String percent) {
+        try {
+            return Integer.parseInt(percent);
+        } catch (NumberFormatException e) {
+            log.warning("Can't parse battery percent!");
+            return 0;
+        }
     }
 
 }
